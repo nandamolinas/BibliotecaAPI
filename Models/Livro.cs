b6d21fe17372068116414a1fd1f4452623354a36
Pedro Bahry <141736881+Bahryz@users.noreply.github.com>
2024-10-09 13:59:56 -0300

    1

---
+++ Models/Livro.cs
@@ -0,0 +1,8 @@
+public class Livro
+{
+    public int Id { get; set; }             
+    public string? Titulo { get; set; }      
+    public string? Autor { get; set; }       
+    public int AnoPublicacao { get; set; }   
+    public string? Genero { get; set; }       
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using BibliotecaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o SQLite e o DbContext
+builder.Services.AddDbContext<BibliotecaContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Execute as migrações, se necessário
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BibliotecaContext>();
+    dbContext.Database.Migrate(); // Aplica migrações pendentes
+}
+
+
+// Maria
+// - Cadastro de Clientes
+// - Consulta de Clientes
+// - Atualização de Dados de Clientes
+
+// Manu
+// - Exclusão de Clientes
+// - Cadastro de Livros
+// - Consulta de Livros
+
+// !!!! Pedro: manu já criei a tabela de livros na pasta models pra fazer a minha parte cria em cima disso!
+
+// Guilherme
+// - Atualização de Dados de Livros
+// - Exclusão de Livros
+// - Registro de Empréstimos
+
+// Pedro
+// - Registro de Devoluções
+// - Consulta de Empréstimos Ativos
+// - Reservas de Livros
+
+app.Run();
